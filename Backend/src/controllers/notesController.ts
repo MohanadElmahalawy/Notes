a5d1a3181782c4046008e7b2254bc5a708f05518
MohanadElmahalawy <mohanadtariq12@gmail.com>
2025-08-16 14:32:26 +0300

    feat: getNodeById controller

--- Backend/src/controllers/notesController.ts
+++ Backend/src/controllers/notesController.ts
@@ -3,13 +3,27 @@ import Note from "../models/Note";
 
 export const getAllNotes = async (req: Request, res: Response) => {
   try {
-    const notes = await Note.find({});
+    const notes = await Note.find().sort({ createdAt: -1 });
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error fetching notes:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getNoteById = async (req: Request, res: Response) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) {
+      res.status(404).json({ message: "Note not found" });
+    }
+    res.status(200).json(note);
+  } catch (error) {
+    console.error("Error fetching note:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 export const createNotes = async (req: Request, res: Response) => {
   try {
     const { title, content } = req.body;
