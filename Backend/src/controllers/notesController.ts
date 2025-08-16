1a8dce86ce19495c6e73e560a601b9073ab91710
MohanadElmahalawy <mohanadtariq12@gmail.com>
2025-08-16 12:28:31 +0300

    feat: getting all notes

--- Backend/src/controllers/notesController.ts
+++ Backend/src/controllers/notesController.ts
@@ -1,4 +1,16 @@
 import { Request,Response } from "express";
-export const getNotes = (req: Request,res: Response)=>{
-    res.send("Notes API is running...");
+import Note from "../models/Note";
+export const getNotes = async (req: Request,res: Response)=>{
+    try{
+        const notes = await Note.find({});
+        res.status(200).json(notes);
+    }
+    catch(error) {
+        console.error("Error fetching notes:", error);
+        res.status(500).json({ message: "Internal server error" });
+
+    }
+    
 } 
+
+
