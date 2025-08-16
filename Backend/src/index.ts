52c680d691d8ba221c9ab0757b26978fafee4de4
MohanadElmahalawy <mohanadtariq12@gmail.com>
2025-08-16 15:25:18 +0300

    fix: connecting to database first then starting server

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,16 +2,20 @@ import express from "express";
 import notesRoute from "./routes/notesRoute";
 import { connectDB } from "./config/db";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use(rateLimiter);//Middleware to limit the request 
+
 const PORT = process.env.PORT || 5000;
-connectDB();
 
 app.use("/api/notes", notesRoute);
 
-app.listen(PORT, () => {
-  console.log("server started at port", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("server started at port", PORT);
+  });
 });
