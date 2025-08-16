adcb1cab8066a9d02632cf48751f8405157cecf3
MohanadElmahalawy <mohanadtariq12@gmail.com>
2025-08-16 12:29:15 +0300

    feat: creating notes

--- Backend/src/controllers/notesController.ts
+++ Backend/src/controllers/notesController.ts
@@ -12,5 +12,17 @@ export const getNotes = async (req: Request,res: Response)=>{
     }
     
 } 
+export const createNotes= async (req:Request,res:Response)=>{
+   try{
+    const {title,content} = req.body;
+    const newNote = new Note({title,content});
+    const savedNote=await newNote.save();
+    res.status(201).json(savedNote);
 
+   }
+   catch(error){
+    console.error("Error creating note:", error);
+    res.status(500).json({ message: "Internal server error" });
 
+   }
+}
